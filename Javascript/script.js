c7506980088a914f761e88b9b11db7c7dc08997d
Mohammadebrahim_2005 <mohammadebrahim340720@gmail.com>
2025-04-14 21:04:23 +0330

    Add theme Dark

--- Javascript/script.js
+++ Javascript/script.js
@@ -6,39 +6,61 @@ const ul = document.querySelector(".todos");
 const filter = document.querySelector(".filter");
 const btnFilter = document.querySelector("#clear-completed");
 
-function main() {
+function main() 
+{
+  themeSwitcherBtn.addEventListener("click", () => 
+    {
+    bodyTag.classList.toggle("light");
+    const themeImg = themeSwitcherBtn.children[0];
+    themeImg.setAttribute(
+      "src",
+      themeImg.getAttribute("src") === "./assets/images/icon-sun.svg" ?
+      "./assets/images/icon-moon.svg" :
+      "./assets/images/icon-sun.svg"
+    );
+  });
+
+
   makeTodoElement(JSON.parse(localStorage.getItem("todos")));
 
-  ul.addEventListener('dragover', (e) => {
+  ul.addEventListener('dragover', (e) => 
+    {
     e.preventDefault();
     if (e.target.classList.contains("card") &&
-      !e.target.classList.contains("dragging")) {
+      !e.target.classList.contains("dragging")) 
+      {
       const draggingCard = document.querySelector(".dragging");
       const cards = [...ul.querySelectorAll(".card")];
       const currentPos = cards.indexOf(draggingCard);
       const newPos = cards.indexOf(e.target);
       console.log(currentPos, newPos);
-      if (currentPos > newPos) {
+      if (currentPos > newPos) 
+        {
         ul.insertBefore(draggingCard, e.target);
-      } else {
-        ul.insertBefore(draggingCard, e.target.nextSibling)
       }
+     else 
+     {
+        ul.insertBefore(draggingCard, e.target.nextSibling)
+     }
       const todos = JSON.parse(localStorage.getItem("todos"));
       const removed = todos.splice(currentPos, 1);
       todos.splice(newPos, 0, removed[0]);
       localStorage.setItem("todos", JSON.stringify(todos));
+
     }
+
   });
 
-  // Add Todo In LocalStorage
-  addBtn.addEventListener("click", () => {
+  addBtn.addEventListener("click", () => 
+    {
     const item = todoInput.value.trim();
     if (item) {
       todoInput.value = "";
       const todos = !localStorage.getItem("todos") ? [] :
         JSON.parse(localStorage.getItem("todos"));
 
-      const currentTodo = {
+      const currentTodo = 
+      {
         item: item,
         isCompleted: false,
       };
@@ -47,66 +69,96 @@ function main() {
       localStorage.setItem("todos", JSON.stringify(todos));
       makeTodoElement([currentTodo]);
     }
+
   });
 
-  todoInput.addEventListener('keydown', (e) => {
-    if (e.key == 'Enter') {
+
+  todoInput.addEventListener('keydown', (e) => 
+    {
+
+    if (e.key == 'Enter') 
+    {
       addBtn.click();
     }
-  });
 
-  filter.addEventListener('click', (e) => {
+  })
+
+  filter.addEventListener('click', (e) => 
+    {
+
     const id = e.target.id;
-    if (id) {
+    if (id) 
+    {
       document.querySelector(".on").classList.remove("on");
       document.getElementById(id).classList.add("on");
       document.querySelector(".todos").className = `todos ${id}`;
     }
+
+
   });
 
-  btnFilter.addEventListener('click', () => {
+
+  btnFilter.addEventListener('click', () => 
+    {
     var deleteIndexes = [];
-    document.querySelectorAll(".card.checked").forEach((card) => {
+    document.querySelectorAll(".card.checked").forEach((card) => 
+        {
       deleteIndexes.push(
         [...document.querySelectorAll(".todos .card")].indexOf(card)
       );
       card.classList.add("fall");
-      card.addEventListener('animationend', () => {
+      card.addEventListener('animationend', () => 
+        {
         card.remove();
       });
+
     });
 
     removeMultipleTodos(deleteIndexes);
-  });
+
+  })
+
 }
 
-function removeTodo(index) {
+function removeTodo(index) 
+{
   const todos = JSON.parse(localStorage.getItem("todos"));
   todos.splice(index, 1);
   localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-function removeMultipleTodos(indexes) {
+function removeMultipleTodos(indexes) 
+{
   var todos = JSON.parse(localStorage.getItem("todos"));
-  todos = todos.filter((todo, index) => {
+  todos = todos.filter((todo, index) => 
+{
     return !indexes.includes(index);
   });
   localStorage.setItem("todos", JSON.stringify(todos));
+
+
 }
 
-function stateTodo(index, isComplete) {
+
+function stateTodo(index, isComplete) 
+{
   const todos = JSON.parse(localStorage.getItem("todos"));
   todos[index].isCompleted = isComplete;
   localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-function makeTodoElement(todoArray) {
-  if (!todoArray) {
+
+
+function makeTodoElement(todoArray) 
+{
+  if (!todoArray) 
+{
     return null;
   }
   const ItemsLeft = document.querySelector('#items-left');
 
-  todoArray.forEach((todoObject) => {
+  todoArray.forEach((todoObject) => 
+    {
     const card = document.createElement("li");
     const cbContainer = document.createElement("div");
     const cbInput = document.createElement("input");
@@ -121,54 +173,69 @@ function makeTodoElement(todoArray) {
     checkSpan.classList.add("check");
     item.classList.add("item");
     clearBtn.classList.add("clear");
-
     card.setAttribute("draggable", true);
     cbInput.setAttribute("type", "checkbox");
     img.setAttribute("src", "./assets/images/icon-cross.svg");
     img.setAttribute("alt", "Clear It");
     item.textContent = todoObject.item;
 
-    if (todoObject.isCompleted) {
+    if (todoObject.isCompleted) 
+    {
       card.classList.add('checked');
       cbInput.setAttribute('checked', 'checked');
     }
 
-    card.addEventListener('dragstart', () => {
+    card.addEventListener('dragstart', () => 
+    {
       card.classList.add("dragging");
     });
 
-    card.addEventListener('dragend', () => {
+    
+    card.addEventListener('dragend', () => 
+    {
       card.classList.remove("dragging");
     });
+    cbInput.addEventListener('click', () => 
+    {
 
-    cbInput.addEventListener('click', () => {
       const currentCard = cbInput.parentElement.parentElement;
       const checked = cbInput.checked;
       const currentCardIndex = [...document.querySelectorAll(".todos .card")]
         .indexOf(currentCard);
       stateTodo(currentCardIndex, checked);
 
-      checked ? currentCard.classList.add('checked') : currentCard.classList.remove('checked');
+      checked ? currentCard.classList.add('checked') : currentCard.classList.remove('checked')
 
       ItemsLeft.textContent = document.querySelectorAll(
         ".todos .card:not(.checked)"
       ).length;
-    });
 
-    clearBtn.addEventListener('click', () => {
+
+    })
+
+    clearBtn.addEventListener('click', () => 
+    {
+
       const currentCard = clearBtn.parentElement;
       currentCard.classList.add('fall');
       const indexOfCurrentCard = [...document.querySelectorAll(".todos .card")].indexOf(currentCard);
       removeTodo(indexOfCurrentCard);
-      currentCard.addEventListener('animationend', () => {
-        setTimeout(() => {
+      currentCard.addEventListener('animationend', () => 
+    {
+
+
+        setTimeout(() => 
+        {
           currentCard.remove();
           ItemsLeft.textContent = document.querySelectorAll(
             ".todos .card:not(.checked)"
           ).length;
         }, 100);
+
       });
+
     });
+
 
     clearBtn.appendChild(img);
     cbContainer.appendChild(cbInput);
@@ -179,10 +246,12 @@ function makeTodoElement(todoArray) {
 
     document.querySelector(".todos").appendChild(card);
   });
-
   ItemsLeft.textContent = document.querySelectorAll(
     ".todos .card:not(.checked)"
   ).length;
 }
 
+
+
 document.addEventListener("DOMContentLoaded", main);
+
